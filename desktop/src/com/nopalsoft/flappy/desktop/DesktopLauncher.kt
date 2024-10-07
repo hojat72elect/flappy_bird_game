1d8ab94de9345f3c73e08768e316d7e792af46ea
hojat <hojat72elect@gmail.com>
2024-10-07 00:08:48 -0400

    Migrated desktop module to kt.

--- desktop/src/com/nopalsoft/flappy/desktop/DesktopLauncher.kt
+++ desktop/src/com/nopalsoft/flappy/desktop/DesktopLauncher.kt
@@ -1,15 +1,15 @@
-package com.nopalsoft.flappy.desktop;
+package com.nopalsoft.flappy.desktop
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.nopalsoft.flappy.MainFlappyBird;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.nopalsoft.flappy.MainFlappyBird
 
-public class DesktopLauncher {
-    public static void main(String[] arg) {
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.title = "Flappy Bird";
-        config.width = 480;
-        config.height = 800;
-        new LwjglApplication(new MainFlappyBird(), config);
+fun main() {
+    val config = LwjglApplicationConfiguration()
+    with(config){
+        title = "Flappy Bird"
+        width = 480
+        height = 800
     }
+    LwjglApplication(MainFlappyBird(), config)
 }
